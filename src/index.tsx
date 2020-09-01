8a06f27823f93405c336f09e243167d5616691ff
bomeekim <thespringg@naver.com>
2020-09-01 21:27:07 +0900

    간단하게 React Component 구성하기

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
-import * as React from "react";
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from "./App";
+interface AppProps {
+  title: string;
+  color: string;
+}
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+function App(props: AppProps) {
+  return <h1>{props.title}</h1>;
+}
+
+/**
+ * React 엘리먼트를 container DOM에 렌더링하고 컴포넌트에 대한 참조를 반환
+ * 최상위에서 한번만 호출된다.
+ * @param element 컴포넌트 인자
+ * @param container
+ */
+ReactDOM.render(
+  <React.StrictMode>
+    <App title="Woowahan Tech React" color="red" />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
